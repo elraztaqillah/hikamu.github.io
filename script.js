af8c884bca8c77f912b66f4bc2b0fe0b9e63b056
elraz <76187141+hielraz@users.noreply.github.com>
2021-06-20 21:37:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-// #memberikan teks sederhana
-// alert('hatihati di jalan');
-
-// #bisa menginput Text 
-// prompt('nama kamu');
-// var nama = prompt('nama kamu');
-// alert('Selamat Datang ' + nama );
-// confirm(nama);
-
-
-// confirm; untuk mengkonfirmasi, tapi bisa menentukan jika user menekan tombol ok / cancel
-// var tes = confirm('yakin');
-// alert(tes);
-
-// var tes = confirm('yakin');
-// if (tes == true) {
-//     alert('Sampai Jumpa Kembali');
-// } else {
-//     alert('terimakasih');
-// }
-
-// alert('Selamat Datang')
-// var nama = prompt('Silahkan Masukkan Nama Kamu');
-// alert('Hai ' + nama);
-
 alert('Selamat Datang');
 var lagi = true;
 
